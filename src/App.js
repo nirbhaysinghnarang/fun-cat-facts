32c478c3357f5b173dc93a6d8380e21f9bf36f2f
nirbhaysinghnarang <nsn8@cornell.edu>
2021-07-21 23:54:07 +0530

    fin

--- src/App.js
+++ src/App.js
@@ -1,25 +1,73 @@
-import logo from './logo.svg';
 import './App.css';
+import React from 'react';
+import ReactDOM from 'react-dom';
+import cat from './cat.jpeg';
 
+const imgApiKey = '03292e54-e8c6-4f20-ae7f-12fefe722c1c';
+let imgUrl;
+let text;
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="App" id="div-app">
+      <div>
+        <div>
+          <header className="App-header">
+              <h1> Fun Cat Facts</h1>
+          </header>
+          <button className="fetchBtn" onClick={fetchFact}>😼 </button>
+        </div>
+          <img className="catImg" src={cat}/>
+          <br/>
+          <p className="catFact"> catto fact incoming </p>
+      </div>
     </div>
   );
 }
 
+const fetchFact = async () => {
+  console.log('fetching');
+  getImg().then(function(res){
+    imgUrl = res
+    getFact().then(function(res){
+      text = res
+      console.log(imgUrl,text);
+      updateUI()
+
+    },function(err){
+      console.log(err);
+    });
+  },function(err){
+    console.log(err);
+  });
+}
+
+const getImg = async () => {
+  const response = await fetch('https://api.thecatapi.com/v1/images/search');
+  const myJson = await response.json();
+  return myJson[0]["url"];
+}
+
+const getFact = async () => {
+  const response = await fetch('https://meowfacts.herokuapp.com/');
+  const myJson = await response.json();
+  return String(myJson["data"]);
+}
+function updateUI(){
+  const img = <img className="catImg" src={imgUrl} />
+  const fact = <p className="catFact"> {text} </p>
+  const factBlock = (
+    <div>
+      <div>
+        <header className="App-header">
+            <h1> Fun Cat Facts</h1>
+        </header>
+        <button className="fetchBtn" onClick={fetchFact}>😼 </button>
+      </div>
+        <img className="catImg" src={imgUrl} />
+        <br/>
+        {fact}
+    </div>
+  )
+  ReactDOM.render(factBlock,document.getElementById('div-app'));
+}
 export default App;
